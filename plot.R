4523548108900bdb9f6a31e4a03e1bf8b2f64644
jasonfan1997 <fkt8356@gmail.com>
2018-09-15 21:14:31 +0800

    a

---
+++ plot.R
@@ -0,0 +1,20 @@
+library(ggplot2)
+this.dir <- dirname(parent.frame(2)$ofile)
+setwd(this.dir)
+df=read.csv("mcmcforstudent.csv")
+df = subset(df, select = -c(Convience,Nightime,China.Mobile,
+                            China.United.Network,China.Telecommunications.Corporation) )#Single value
+df = subset(df, select = -c(Daytime,theater) )#Single value
+X=df[c(2:23)]
+Y=df[24]
+logy=log(Y)
+col=colnames(X)
+for (i in col)
+{
+  temp=ggplot(df,aes_string(x=i, y="the.eletricity.sell.amount.per.day")) +
+  geom_point(shape=1) +    #
+  geom_smooth(method=lm)
+  fit=lm(Y$the.eletricity.sell.amount.per.day~X[[i]])
+  temp=temp+labs(title=paste("R squred=",(summary(fit))$r.squared))
+  ggsave(paste(i,".png"), plot = last_plot(),path = "./plot/",units='cm',width = 16, height = 9,dpi=500)
+}
--- project.R
+++ project.R
@@ -6,4 +6,23 @@ df = subset(df, select = -c(Convience,Nightime,China.Mobile,
 df = subset(df, select = -c(Daytime,theater) )#Single value
 X=df[c(2:23)]
 Y=df[24]
-fit=lm(Y$the.eletricity.sell.amount.per.day~.,data = X)
+logy=log(Y)
+col=colnames(X)
+t=1
+#t=0
+bestcol=colnames(X)
+for (i in 1:21)
+{
+  fit=lm(logy$the.eletricity.sell.amount.per.day~.,data=X[col])
+  coeff<- coef(summary(fit))
+  f=(summary(fit)$fstatistic)
+  tempt=pf(f[1],f[2],f[3],lower.tail=F)
+  #tempt=(summary(fit)$adj.r.squared)
+  if(tempt<t){
+    t=tempt
+    bestcol=col
+  }
+  worst=which.max(coeff[,"Pr(>|t|)"])-1
+  col=col[-c(worst)]
+    
+}
