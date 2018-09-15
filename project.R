ab2c88e53ca28b6f8c470572e95ae6227c733da8
jasonfan1997 <fkt8356@gmail.com>
2018-09-15 15:03:23 +0800

    aaa

--- project.R
+++ project.R
@@ -1,6 +1,9 @@
+this.dir <- dirname(parent.frame(2)$ofile)
+setwd(this.dir)
 df=read.csv("mcmcforstudent.csv")
 df = subset(df, select = -c(Convience,Nightime,China.Mobile,
                             China.United.Network,China.Telecommunications.Corporation) )#Single value
 df = subset(df, select = -c(Daytime,theater) )#Single value
 X=df[c(2:23)]
 Y=df[24]
+fit=lm(Y$the.eletricity.sell.amount.per.day~.,data = X)
